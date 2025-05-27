3daca8a7d6278d22c20255914c5bffbc904a708e
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-27 14:57:42 +0200

    start to do list

--- assets/script/scriptToDo.js
+++ assets/script/scriptToDo.js
@@ -1,12 +1,7 @@
 function addNewTask (){
     let inputContent = document.querySelector("#inputContent").value;  
     let taskList = document.querySelector("#to-do");
-    let newTask = document.createElement("p");
+    let newTask = document.createElement("span");
     taskList.append(inputContent, newTask);
 }
 
-document.querySelectorAll("#to-do").addEventListener("click", isDone());
-
-function isDone(){
-    
-}
