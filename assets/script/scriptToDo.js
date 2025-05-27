c0c80615ca1ac352c4ab96eee99a3feb48364933
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-27 16:24:40 +0200

    changment des span en p sur le create element du prototype de todo liste

--- assets/script/scriptToDo.js
+++ assets/script/scriptToDo.js
@@ -1,7 +1,7 @@
 function addNewTask (){
     let inputContent = document.querySelector("#inputContent").value;  
     let taskList = document.querySelector("#to-do");
-    let newTask = document.createElement("span");
+    let newTask = document.createElement("p");
     taskList.append(inputContent, newTask);
 }
 
