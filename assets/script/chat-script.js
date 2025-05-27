d2eff875e2ddb97630768997beba699506af3335
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-27 15:35:23 +0200

    mise en place

--- assets/script/chat-script.js
+++ assets/script/chat-script.js
@@ -1,2 +1,6 @@
 let userForce = 10;
 let catLife = 100;
+
+let forceUtilisateur = document.querySelector("#force-utilisateur");
+let vieDuChat = document.querySelector("#vie-du-chat");
+let ImageDuChaton = document.querySelector("#image-du-chaton");
