6bdd94cef822b8294c9605b0623e9affff939d3b
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 13:22:15 +0200

    ajout de la fonction suppression et ajout de commentaires

--- assets/script/scriptToDo.js
+++ assets/script/scriptToDo.js
@@ -2,9 +2,13 @@ let ajouterUneTache = document.querySelector("#addTask");
 let selectionnerToutesLesTaches = document.querySelector("#selectAllTasks");
 let champSaisie = document.querySelector("#inputContent");
 let listeDeTaches = document.querySelector("#toDoTasks");
+let supprimerUneTache = document.querySelector("#suppressTask");
 
 /*
- * Listener ajoute une tâche au clique
+Listener ajoute une tâche au clique sur le bouton "Ajouter"("#addTask")
+Commence par vérifier que l'input ("#inputContent") n'est pas vide
+Appel la fonction createListElement() pour créer un nouvel élément
+cliquable dans la liste.
  */
 ajouterUneTache.addEventListener('click', () => {
     if(champSaisie.value.length >0){
@@ -23,13 +27,25 @@ selectionnerToutesLesTaches.addEventListener('click', () => {
     }
 })
 
+/* Supprimer une ou des tâches en se basant sur le style
+.done-style qui indique qu'un élément est "fait"
+*/
+supprimerUneTache.addEventListener('click', () => {
+    let listeTacheRealisees = document.querySelectorAll(".done-style");
+    for (let i = 0 ; i < listeTacheRealisees.length ; i++){
+        listeTacheRealisees[i].remove();
+    }
+})
 
-// créer un élément de la liste
+/* créer un élément de la liste et au clique sur cet élément
+lui appliquer le style done-style (ou retourner au style de base si
+done_style est déjà appliqué)
+*/
 function createListElement(){
         let nouvelleTache = document.createElement("li");
         nouvelleTache.innerHTML = champSaisie.value;   
         nouvelleTache.addEventListener('click', () =>{
-            nouvelleTache.classList.toggle('done-style');
+        nouvelleTache.classList.toggle('done-style');
         });
         return nouvelleTache;
 }
