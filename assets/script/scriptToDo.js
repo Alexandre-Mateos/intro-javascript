6bbd8245440fbfa77566f589d3879ac1cb1d4613
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 11:18:04 +0200

    ajout bouton tout selectionner

--- assets/script/scriptToDo.js
+++ assets/script/scriptToDo.js
@@ -3,18 +3,28 @@
 addTask.addEventListener('click', () => {
     let inputContent = document.querySelector("#inputContent").value;  
     let taskList = document.querySelector("#toDoTasks");
-    let newTask = document.createElement("li");
-    let taskContent = document.createTextNode (inputContent);
-
-    newTask.addEventListener('click', () =>{
-        const newTaksClassLists = newTask.classList;
-        if (newTaksClassLists.toggle("doneStyle")){
-            newTask.classList.add("doneStyle");
-        }
-    });
-
-    newTask.appendChild(taskContent);
-    taskList.appendChild(newTask);
-    newTask.classList.add("a-faire");
+    if(inputContent.length >0){
+        let newTask = document.createElement("li");
+        let taskContent = document.createTextNode (inputContent);
+        
+        newTask.addEventListener('click', () =>{
+            const newTaksClassLists = newTask.classList;
+            if (newTaksClassLists.toggle("done-style")){
+                newTask.classList.add("done-style");
+            }
+        });
+        
+        newTask.appendChild(taskContent);
+        taskList.appendChild(newTask);
+        newTask.classList.add("a-faire");
+        document.querySelector("#inputContent").value = "";
+    }
 });
+
+selectAllTasks.addEventListener('click', () => {
+    let toDoLists = document.querySelectorAll("li");
+    for (let i = 0 ; i < toDoLists.length ; i++){
+        toDoLists[i].classList.add("done-style");
+    }
+})
 
