ba10f9ccf539d5a49793f7adc8ad2ede81e6d7aa
alexandre-mateos <mateosalexandre95@gmail.com>
2025-05-28 10:38:13 +0200

    ajout de la fonction premettant d'indiquer les tâches réalisées

--- assets/script/scriptToDo.js
+++ assets/script/scriptToDo.js
@@ -1,12 +1,20 @@
-function addNewTask (){
+
+
+addTask.addEventListener('click', () => {
     let inputContent = document.querySelector("#inputContent").value;  
-    let taskList = document.querySelector("#to-do");
-    let newTask = document.createElement("p");
+    let taskList = document.querySelector("#toDoTasks");
+    let newTask = document.createElement("li");
     let taskContent = document.createTextNode (inputContent);
+
+    newTask.addEventListener('click', () =>{
+        const newTaksClassLists = newTask.classList;
+        if (newTaksClassLists.toggle("doneStyle")){
+            newTask.classList.add("doneStyle");
+        }
+    });
+
     newTask.appendChild(taskContent);
     taskList.appendChild(newTask);
-}
-
-
-
+    newTask.classList.add("a-faire");
+});
 
